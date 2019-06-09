ce786dba80501af6cf7bcb4fec9c1a114c153766
github username <github email address>
2019-06-09 22:00:06 +0100

    Functions with return statements using -> Int syntax created

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,39 +1,25 @@
-func printAge(name: String) {
-    print("\(name) is 29")
-}
-
-printAge("Adam")
-
-
-func happyBirthday(age: Int) {
-    print("Happy birthday #\(age)!")
-}
-
-
 func getAge(name: String) -> Int {
     return 29
 }
 
-// Uncomment to see the error
-/*
-func badGetAge(name: String) -> Int {
-    print("\(name) is 29")
-}
-*/
+getAge(name: "Mike")
 
+//func badGetAge(name: String) -> Int {
+//    print("\(name) is 29")
+//}
+
+//badGetAge(name: "Marc")
 
 let friend = "Emily"
-var friendAge = getAge(friend)
-happyBirthday(friendAge)
-
+var friendAge = getAge(name: friend)
+//
+//happyBirthday(age: friendAge)
 
 func birthdayGreeting(age: Int) -> String {
     return "Happy birthday #\(age)!"
 }
 
-var greeting = birthdayGreeting(30)
-print(greeting)
-
+birthdayGreeting(age: 30)
 
 func getAgeAndCongratulate(name: String) -> Int {
     let age = 29
@@ -42,4 +28,52 @@ func getAgeAndCongratulate(name: String) -> Int {
 }
 
 let friend2 = "Pete"
-var friend2Age = getAgeAndCongratulate(friend2)
+var friend2Age = getAgeAndCongratulate(name: friend2)
+
+//
+//func printAge(name: String) {
+//    print("\(name) is 29")
+//}
+//
+//printAge("Adam")
+//
+//
+//func happyBirthday(age: Int) {
+//    print("Happy birthday #\(age)!")
+//}
+//
+//
+//func getAge(name: String) -> Int {
+//    return 29
+//}
+//
+//// Uncomment to see the error
+///*
+//func badGetAge(name: String) -> Int {
+//    print("\(name) is 29")
+//}
+//*/
+//
+//
+//let friend = "Emily"
+//var friendAge = getAge(friend)
+//happyBirthday(friendAge)
+//
+//
+//func birthdayGreeting(age: Int) -> String {
+//    return "Happy birthday #\(age)!"
+//}
+//
+//var greeting = birthdayGreeting(30)
+//print(greeting)
+//
+//
+//func getAgeAndCongratulate(name: String) -> Int {
+//    let age = 29
+//    print("Happy \(age)th birthday, \(name)!")
+//    return age
+//}
+//
+//let friend2 = "Pete"
+//var friend2Age = getAgeAndCongratulate(friend2)
+
